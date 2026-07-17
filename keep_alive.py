96f64a1d099862becd5e76924ceed21307c7ce56
hmody7442-a11y <hmody7442@gmail.com>
2026-07-17 06:15:05 +0300

    Add /api/ping route to keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -7,6 +7,10 @@ app = Flask('')
 def home():
     return "Bot is alive and running!"
 
+@app.route('/api/ping')
+def ping():
+    return "pong"
+
 def run():
     app.run(host='0.0.0.0', port=8000)
 
